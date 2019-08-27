5522aa30adda36c78b67638bb5d3a54d01159c65
Nacera HAMMID <cilacila4@gmail.com>
2019-08-27 12:15:03 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Get “bands/search” , to: "bands#search"
-  Get “bands/filter” , to: "bands#filter"
-  Get “dashboard” ,  to: "pages#dashboard"
-  Resources : conversations , only [:index, :show, :create] do {
-  Resources :messages , only [:create]
-  }
-  Get “users/search”, to: "users#search"
-  Get “users/filter” , to: "users#filter"
-  Resources : bands , only [:index, :show, :edit, :create, :update, :new]
+  get "bands/search" , to: "bands#search"
+  get "bands/filter" , to: "bands#filter"
+  get "dashboard" ,  to: "pages#dashboard"
+  resources :conversations , only: [:index, :show, :create] do
+    resources :messages , only: [:create]
+  end
+  get "users/search", to: "users#search"
+  get "users/filter" , to: "users#filter"
+  resources :bands , only: [:index, :show, :edit, :create, :update, :new]
 end
