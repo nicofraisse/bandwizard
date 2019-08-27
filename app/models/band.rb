534bbcf83ce127f372b6d0c292255063cb464967
Nacera HAMMID <cilacila4@gmail.com>
2019-08-27 11:35:36 -0400

    Relationships Added

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,9 @@
 class Band < ApplicationRecord
+  has_many :photos
+  has_many :needed_instruments
+  has_many :instruments, through: :needed_instruments
+  has_many :style_bands
+  has_many :styles, through: :style_bands
+  has_many :ffavorite_band_artists
+  has_many :favorite_artists, through: :favorite_band_artists
 end
