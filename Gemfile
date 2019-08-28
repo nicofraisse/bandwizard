d50b78c9128f8d24e68286df81ce28e58065836a
Nacera HAMMID <cilacila4@gmail.com>
2019-08-28 15:33:52 -0400

    done for band

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
-
+gem 'geocoder'
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,4 +1,13 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.where(nil) # creates an anonymous scope
+    filtering_params(params).each do |key, value|
+    @bands = @bands.public_send(key, value) if value.present?
+  end
+  render "results"
+  end
+
+
   def new
     @band = Band.new
     @band_photo = @band.band_photos.build
@@ -11,10 +20,6 @@ class BandsController < ApplicationController
        params[:band_photos]['photo'].each do |a|
           band_photo = band.band_photos.create!(:photo => a, :band_id => band.id)
        end
-
-
-
-
       redirect_to band_path(band)
     else
       render :new
@@ -24,9 +29,14 @@ class BandsController < ApplicationController
   def show
     @band = Band.find(params[:id])
   end
+
 private
 def band_params
   params.require(:band).permit(:name, :bio,:personal_website, :youtube_link, :address, :soundcloud_link, :is_recording,:is_pro,:is_live,:is_jamming, :is_cover,:is_pro,:is_composition,band_photos_attributes:
   [:id, :band_id, :photo])
 end
+def filtering_params(params)
+  params.slice(:instruments, :styles, :address)
+end
+
 end
