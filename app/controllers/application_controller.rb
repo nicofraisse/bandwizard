19b26a971a755e6de3d2c29aea2b67b4d39e7933
Nacera HAMMID <cilacila4@gmail.com>
2019-08-31 11:42:43 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: [:index, :search], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :search], unless: :skip_pundit?
 
   private
 
