63f40160db404473342841f2293bd1323f6e156b
Nacera HAMMID <cilacila4@gmail.com>
2019-09-01 07:18:15 -0400

    100%search 70%map 0%Position

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home,:public_profile]
 
   def home
   end
