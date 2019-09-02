898482c1880a0ef935ae2ea895e753e0f32b0d73
Luc Mosser <luc.mosser86@orange.fr>
2019-09-02 18:01:43 -0400

    style

---
+++ app/javascript/plugins/select2.js
@@ -0,0 +1,10 @@
+import "bootstrap";
+import $ from "jquery";
+import "select2";
+
+console.log('hello from select 2')
+
+$(document).ready(function() {
+    $('.search-band-select').select2();
+});
+
---
+++ app/javascript/plugins/slider.js
@@ -0,0 +1,17 @@
+console.log('hello from double slider')
+
+// import noUiSlider from 'nouislider'
+// var doubleSlider = document.getElementById('double-slider');
+
+// console.log(doubleSlider)
+
+// noUiSlider.create(doubleSlider, {
+//     start: [20, 80],
+//     connect: true,
+//     range: {
+//         'min': 0,
+//         'max': 100
+//     }
+// });
+
+
