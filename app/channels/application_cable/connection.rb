de4227805ea659a4f9de6cb723844dc14d759bba
Richard Jarram <rjarram@me.com>
2019-09-02 15:07:28 -0400

    refactored action cable code

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,27 +3,10 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      # true
-      # self.current_user
       self.current_user = find_verified_user
-      # logger.add_tags 'ActionCable', current_user.name
     end
 
     def find_verified_user
-      # binding.pry
-
-      # puts "session in cable start"
-      # # puts cookies.signed.size
-      # puts cookies.signed[:user_id]
-      # puts cookies.encrypted[:user_id]
-      # puts "session in cable end"
-      # p "my user session"
-      # p UserSession.last.identifier
-      # puts "session" + session[:user_id].to_s
-      # puts cookies.signed['user_id']
-
-      # User.find(4)
-
       verified_user = User.find(UserSession.first.identifier.to_i)
       if verified_user
         verified_user
