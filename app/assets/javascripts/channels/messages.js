d3929c6771df8f0d82983f8fef58a65524060e0b
Nacera HAMMID <cilacila4@gmail.com>
2019-09-03 17:43:35 -0400

    messages cleared once sent , scroll down when sent

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,7 +1,7 @@
 App.messages = App.cable.subscriptions.create({ channel:'MessagesChannel',user_id: document.querySelector("meta[name='user-id']").content}, {
   received: function(data) {
-    console.log('received', data);
     document.querySelector("#messages").classList.remove('hidden')
-    return document.querySelector('#messages').insertAdjacentHTML("beforeend", data.messageHTML);
+    document.querySelector('#messages').insertAdjacentHTML("beforeend", data.messageHTML);
+    scrollConvoDown();
   }
 });
