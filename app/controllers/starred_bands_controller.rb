782fa5ad0362544c0d7ca0e5871a0366ffc9e864
Nacera HAMMID <cilacila4@gmail.com>
2019-09-04 17:27:33 -0400

    done for instant delete

--- app/controllers/starred_bands_controller.rb
+++ app/controllers/starred_bands_controller.rb
@@ -1,16 +1,16 @@
 class StarredBandsController < ApplicationController
   def create
-    favorite = StarredBand.where(band:Band.find(params[:band_id]),user:current_user)
+    @favorite = StarredBand.find_by(band: Band.find(params[:band_id]), user: current_user)
     @starred = StarredBand.new
     authorize @starred
     @band = Band.find(params[:band_id])
-    if favorite == []
+    if @favorite.nil?
       @starred.band = @band
       @starred.user = current_user
       @starred.save!
       @favorite_exists = true
     else
-      favorite.destroy_all
+      @favorite.destroy
       @favorite_exists = false
     end
     respond_to do |format|
